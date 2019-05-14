440c070af279d6016b00777c34ad6c6533240dfa
paul wang <wangj@abc-group.cn>
2019-05-14 17:27:16 +0800

    获取文章sql语句修改和删除文章关联文章评论表

--- server/sql/article.js
+++ server/sql/article.js
@@ -4,10 +4,14 @@ const ARTICLE_TABLE_NAME = 'article_info';
 const ARTICLE_COMMENT_TABLE_NAME = 'article_comment';
 const article = {
 	async getArticleAllList() {
-		return query(`SELECT * FROM ${ARTICLE_TABLE_NAME} a LEFT JOIN ${ARTICLE_COMMENT_TABLE_NAME} b ON a.id = b.articleId LEFT JOIN user_info c ON b.userId = c.id;`)
+		return query(`SELECT a.*, b.id commentId, b.content commentContent, b.createTime commentCreateTime, c.name userName 
+		FROM ${ARTICLE_TABLE_NAME} a LEFT JOIN ${ARTICLE_COMMENT_TABLE_NAME} b 
+		ON a.id = b.articleId LEFT JOIN user_info c ON b.userId = c.id;`)
 	},
 	async getArticleById(id) {
-		return query(`select * from ${ARTICLE_TABLE_NAME} where id=${id}`)
+		return query(`SELECT a.*, b.id commentId, b.content commentContent, b.createTime commentCreateTime, c.name userName 
+		FROM ${ARTICLE_TABLE_NAME} a LEFT JOIN ${ARTICLE_COMMENT_TABLE_NAME} b 
+		ON a.id = b.articleId LEFT JOIN user_info c ON b.userId = c.id where a.id=${id};`)
 	},
 	async createArticle(data) {
 		let sqlStatement = `insert into ${ARTICLE_TABLE_NAME} (title, categories, tagIds, content, updateTime, createTime) values (?, ?, ?, ?, ?, ?)`;
@@ -21,7 +25,7 @@ const article = {
 		return query(`update ${ARTICLE_TABLE_NAME} set title='${data.title}', categories='${data.categories}', tagIds='${data.tagIds}', content='${content}', updateTime='${currentDate}' where id = ${id}`)
 	},
 	async deleteArticle(id) {
-		return query(`delete a,b from ${ARTICLE_TABLE_NAME} a inner join ${ARTICLE_COMMENT_TABLE_NAME} b where a.id = b.articleId and a.id = ${id}`)
+		return query(`delete ${ARTICLE_TABLE_NAME}, ${ARTICLE_COMMENT_TABLE_NAME} from ${ARTICLE_TABLE_NAME} left join ${ARTICLE_COMMENT_TABLE_NAME} on ${ARTICLE_TABLE_NAME}.id = ${ARTICLE_COMMENT_TABLE_NAME}.articleId where ${ARTICLE_TABLE_NAME}.id = ${id}`)
 	},
 	async createArticleComment(id, data) {
 		let sqlStatement = `insert into ${ARTICLE_COMMENT_TABLE_NAME} (articleId, userId, content, createTime) values (?, ?, ?, ?)`;
@@ -29,7 +33,7 @@ const article = {
 		return query(sqlStatement, [id, data.userId, data.content, currentDate])
 	},
 	async getArticleCommentList(id) {
-		return query(`select * from ${ARTICLE_COMMENT_TABLE_NAME} where articleId=${id}`)
+		return query(`select a.*, b.name userName from ${ARTICLE_COMMENT_TABLE_NAME} a left join user_info b ON a.userId = b.id where a.articleId=${id}`)
 	}
 };
 module.exports = article;
