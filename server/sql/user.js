cbd08fdce9a58bdc73711d56b7e641abd8dff6e8
paul wang <wangj@abc-group.cn>
2019-05-23 16:05:23 +0800

    修改user sql 语句并添加修改用户sql语句

--- server/sql/user.js
+++ server/sql/user.js
@@ -1,18 +1,23 @@
 const {query} = require('../utils/async-db');
 const USER_TABLE_NAME = 'user_info';
 const user = {
-	async createUser(users) {
-		let sqlStatement = `insert into ${USER_TABLE_NAME} (name, password) values (?, ?)`;
-		return query(sqlStatement, [users.userName, users.password])
+	async registerUser(users) {
+		let createTime = new Date().toLocaleString();
+		let sqlStatement = `insert into ${USER_TABLE_NAME} (email, username, password, createTime) values (?, ?, ?, ?)`;
+		return query(sqlStatement, [users.email, users.userName, users.password, createTime])
 	},
 	async queryUseExists(users) {
-		return query(`select * from ${USER_TABLE_NAME} where BINARY name='${users.username}' and password='${users.password}'`)
+		return query(`select * from ${USER_TABLE_NAME} where BINARY email='${users.email}' and password='${users.password}'`)
 	},
 	async getUserList(params) {
 		return query(`select * from ${USER_TABLE_NAME} where name like '%${params.name}%' limit ${params.limit} offset ${params.offset}`)
 	},
 	async getUserInfo(id) {
-		return query(`select * from ${USER_TABLE_NAME} where id='${id}'`)
+		return query(`select * from ${USER_TABLE_NAME} where id=${id}`)
+	},
+	async updateUser(id, users) {
+		let updateTime = new Date().toLocaleString();
+		return query(`update ${USER_TABLE_NAME} set email='${users.email}', username='${users.username}', password='${users.password}', updateTime='${updateTime}' where id = ${id}`)
 	}
 };
 
