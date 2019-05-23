c11599d264f507ba1a7bcaa72eb0c3431a5ddd7d
paul wang <wangj@abc-group.cn>
2019-05-23 16:03:52 +0800

    user controller 添加user joi模块，新增修改user信息和注册用户功能

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,46 +1,43 @@
+const Joi = require('joi');
+const UserSchema = require('../schemas/user');
+
+const createResponse = require('../utils/create-response');
 const userSql = require('../sql/user');
 const { createToken } = require('../utils/check-token');
 const user = {
 	async login(ctx) {
-		let response = {
-			code: 0,
-			result: null,
-			message: null
-		};
-		let {username, password} = ctx.request.body;
-		if (!username || !password) {
-			response.code = 400;
-			response.message = '请输入用户名或密码';
-			return ctx.body = response;
+		const requestBody = ctx.request.body;
+		const response = createResponse();
+		const {email, password} = requestBody;
+		const validator = Joi.validate(requestBody, UserSchema.login);
+		if (validator.error) {
+			return ctx.body = {code: 400, message: validator.error.message}
 		}
-		let res = await userSql.queryUseExists({username, password});
+		let res = await userSql.queryUseExists({email, password});
 		if (res && res.length) {
 			response.message = '成功';
-			response.result = Object.assign({}, res[0], {
-				token: createToken(Object.assign({}, res[0]))
-			})
+			const userInfo = res[0];
+			response.result = Object.assign({}, userInfo, {token: createToken(Object.assign({}, userInfo))});
+			delete response.result.password;
 		} else {
 			response.code = 400;
-			response.message = '用户不存在';
+			response.message = '账号或密码错误';
 		}
 		ctx.body = response;
 	},
-	async createUser(ctx) {
-		let requestBody = ctx.request.body;
-		let response = {
-			code: 0,
-			result: null,
-			message: null
-		};
-		if (!requestBody.userName) {
-			response.message = '请输入用户名';
-			return ctx.body = response
+	async registerUser(ctx) {
+		const requestBody = ctx.request.body;
+		const response = createResponse();
+		const {email, username, password} = requestBody;
+		const validator = Joi.validate(requestBody, UserSchema.registerUser);
+		if (validator.error) {
+			return ctx.body = {code: 400, message: validator.error.message}
 		}
-		if (!requestBody.password) {
-			response.message = '请输入用户密码';
-			return ctx.body = response
+		const findUser = await userSql.queryUseExists({email, password});
+		if (findUser && findUser.length > 0) {
+			return ctx.body = {code: 400, message: '当前用户已注册'}
 		}
-		let res = await userSql.createUser(requestBody);
+		let res = await userSql.registerUser({email, username, password});
 		if (res && res.insertId - 0 > 0) {
 			response.message = '成功';
 		}
@@ -68,6 +65,36 @@ const user = {
 			response.code = 404;
 			response.message = '信息不存在';
 		}
+		ctx.body = response;
+	},
+
+	async updateUser(ctx) {
+		const id = ctx.params.id;
+		const requestBody = ctx.request.body;
+		const response = createResponse();
+		const {email, username, password, oldPassword} = requestBody;
+		const validator = Joi.validate(requestBody, UserSchema.updateUser);
+		if (validator.error) {
+			return ctx.body = {code: 400, message: validator.error.message}
+		}
+
+		const findUser = await userSql.queryUseExists(oldPassword ? {email, password: oldPassword} : {email, password});
+		let findUserInfo;
+		if (findUser && findUser.length > 0) {
+			findUserInfo = findUser[0];
+		} else {
+			return ctx.body = {code: 400, message:  `${oldPassword ? '原' : ''}密码错误`}
+		}
+
+		let res = await userSql.updateUser(id, {email, username, password});
+		if (res && res.insertId - 0 > 0) {
+			response.message = `成功`;
+		}
+		//更改用户最新信息后获取最新用户信息详情
+		const updateUser = await userSql.getUserInfo(id);
+		const updateUserInfo = updateUser[0];
+		response.result = Object.assign({}, updateUserInfo, {token: createToken(Object.assign({}, updateUserInfo))});
+		delete response.result.password;
 		ctx.body = response;
 	}
 };
