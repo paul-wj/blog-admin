169e25433130bf6452fa686c62efc05c8785e735
paul wang <wangj@abc-group.cn>
2019-05-24 18:11:39 +0800

    修改数据库结构

--- init/sql/article_reply.sql
+++ init/sql/article_reply.sql
@@ -1,9 +1,10 @@
 CREATE TABLE  IF NOT EXISTS `article_reply` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `commentId` int(11) DEFAULT NULL COMMENT '评论Id',
-  `userId` int(11) DEFAULT NULL COMMENT '回复人',
-  `toUserId` int(11) DEFAULT NULL COMMENT '回复对象',
-  `content` longtext DEFAULT NULL COMMENT '回复内容',
-  `createTime` varchar(20) DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`)
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+  `commentId` int(11) NULL DEFAULT NULL COMMENT '文章评论id',
+  `userId` int(11) NULL DEFAULT NULL COMMENT '评论用户id',
+  `toUserId` int(11) NULL DEFAULT NULL COMMENT '回复用户id',
+  `content` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '回复内容',
+  `type` int(11) NULL DEFAULT NULL COMMENT '回复类型（10：点赞，20：踩,  30: 文字回复）',
+  `createTime` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
