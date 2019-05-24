f5edd536fd6b5a1547fce823440e9917e192a906
paul wang <wangj@abc-group.cn>
2019-05-24 18:12:06 +0800

    添加全局中间件koa-helmet

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // require('./init');
 const Koa = require('koa');
-const helmet = require('helmet');
+const helmet = require('koa-helmet');
 const bodyParser = require('koa-bodyparser');
 const logger = require('./server/utils/logger');
 const cors = require('./server/utils/set-cors');
 const {checkToken} = require('./server/utils/check-token');
 const errorHandle  = require('./server/utils/errorHandle');
-
 const config = require('./config');
 const router = require('./server/routers/index');
 const app = new Koa();
+app.use(helmet());
 // 配置ctx.body解析中间件
 app.use(bodyParser());
 //配置跨域cors
@@ -19,6 +19,5 @@ app.use(checkToken).use(errorHandle);
 // 配置控制台日志中间件
 app.use(logger);
 app.use(router.routes()).use(router.allowedMethods());
-app.use(helmet());
 app.listen(config.port);
 console.log(`starting at port:${config.port}`);
