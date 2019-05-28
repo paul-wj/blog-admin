90f27cfc7e16b4df64257966c33990422dcb5a06
paul wang <wangj@abc-group.cn>
2019-05-28 18:32:48 +0800

    修改数据库article_reply结构

--- init/sql/article_reply.sql
+++ init/sql/article_reply.sql
@@ -1,6 +1,8 @@
 CREATE TABLE  IF NOT EXISTS `article_reply` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
   `commentId` int(11) NULL DEFAULT NULL COMMENT '文章评论id',
+  `replyWay` int(11) NULL DEFAULT NULL COMMENT '回复方式（10: 回复他人评论， 20：回复别人的回复）',
+  `replyId` int(11) NULL DEFAULT NULL COMMENT '回复目标id（replyWay为10时为commentId，replyWay为20时为replyId）',
   `userId` int(11) NULL DEFAULT NULL COMMENT '评论用户id',
   `toUserId` int(11) NULL DEFAULT NULL COMMENT '回复用户id',
   `content` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '回复内容',
