58116af65cf4b6ef41da0b7afc91bc9df5395659
paul wang <wangj@abc-group.cn>
2019-06-19 14:10:51 +0800

    添加koa-body和koa-static全局中间件

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // require('./init');
 const Koa = require('koa');
 const helmet = require('koa-helmet');
+const koaBody = require('koa-body');
 const bodyParser = require('koa-bodyparser');
+const koaStatic = require('koa-static');
+
 const logger = require('./server/utils/logger');
 const cors = require('./server/utils/set-cors');
 const {checkToken} = require('./server/utils/check-token');
@@ -9,7 +12,19 @@ const errorHandle  = require('./server/utils/errorHandle');
 const config = require('./config');
 const router = require('./server/routers/index');
 const app = new Koa();
+
+//配置koa-static静态文件服务器
+app.use(koaStatic(config.STATIC_PATH));
+
 app.use(helmet());
+//配置koa-body中间件
+app.use(koaBody({
+	multipart:true,
+	formidable:{
+		maxFieldsSize:10*1024*1024,
+		multipart:true
+	}
+}));
 // 配置ctx.body解析中间件
 app.use(bodyParser());
 //配置跨域cors
