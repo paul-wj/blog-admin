d5e366697b92bb06214e7b99bfc6d0717e66b489
paul wang <wangj@abc-group.cn>
2019-06-19 14:11:18 +0800

    cros添加所支持的header类型

--- server/utils/set-cors.js
+++ server/utils/set-cors.js
@@ -9,7 +9,7 @@ const crosOptions = {
 	maxAge: 60*60,                                                                      //设置预检请求有效期（单位: s）
 	credentials: false,                                                               //是否允许传输cookie（如果为true需要保证origin为*）
 	allowMethods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'], //设置请求支持的类型
-	allowHeaders: ['Content-Type', 'Authorization', 'Accept'],                   //请求服务器支持的所有头信息字段
+	allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-requested-with'],                   //请求服务器支持的所有头信息字段
 };
 module.exports = cors(crosOptions);
 
