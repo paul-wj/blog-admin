635d27775a6e5e32583674ca65d75916e9bfbf8a
paul wang <wangj@abc-group.cn>
2019-07-04 14:29:10 +0800

    log表添加origin和userAgent列

--- init/sql/log.sql
+++ init/sql/log.sql
@@ -3,6 +3,8 @@ CREATE TABLE  IF NOT EXISTS `log_info` (
   `userName` varchar(255) DEFAULT NULL,
   `userId` int(11) DEFAULT NULL,
   `host` varchar(50) DEFAULT NULL,
+  `origin` varchar(50) DEFAULT NULL,
+  `userAgent` varchar(50) DEFAULT NULL,
   `status` varchar(20) DEFAULT NULL,
   `url` varchar(20) DEFAULT NULL,
   `method` varChar(20)  DEFAULT NULL,
