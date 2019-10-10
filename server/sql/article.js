944ed458906619d6432ae8d328073c1327b3aba1
paul wang <wangj@abc-group.cn>
2019-10-10 17:59:16 +0800

    服务器mysql升级为8，sql语句不兼容修改

--- server/sql/article.js
+++ server/sql/article.js
@@ -4,14 +4,14 @@ const databaseNameList = require('../../config/index').databaseNameList;
 const {ARTICLE_TABLE_NAME, ARTICLE_COMMENT_TABLE_NAME, ARTICLE_REPLY_TABLE_NAME, USER_TABLE_NAME} = databaseNameList;
 const article = {
 	async getArticleAllList() {
-		return query(`select * from ${ARTICLE_TABLE_NAME} group by createTime desc`)
+		return query(`select * from ${ARTICLE_TABLE_NAME} order by createTime desc`)
 		// return query(`SELECT a.*, b.id commentId, b.content commentContent, b.createTime commentCreateTime, c.username userName
 		// FROM ${ARTICLE_TABLE_NAME} a LEFT JOIN ${ARTICLE_COMMENT_TABLE_NAME} b
 		// ON a.id = b.articleId LEFT JOIN ${USER_TABLE_NAME} c ON b.userId = c.id;`)
 	},
 	async getArticlePageList(params) {
 		const {limit, offset, title} = params;
-		return query(`select sql_calc_found_rows  * from ${ARTICLE_TABLE_NAME} where title like '%${title || ''}%' group by createTime desc limit ${limit} offset ${offset};SELECT FOUND_ROWS() as total;`)
+		return query(`select sql_calc_found_rows  * from ${ARTICLE_TABLE_NAME} where title like '%${title || ''}%' order by createTime desc limit ${limit} offset ${offset};SELECT FOUND_ROWS() as total;`)
 	},
 	async getArticleById(id) {
 		return query(`select * from ${ARTICLE_TABLE_NAME} where id=${id}`);
@@ -21,11 +21,11 @@ const article = {
 	},
 	async getArticlePageListByCategoryId(params) {
 		const {categoryId, limit, offset} = params;
-		return query(`select sql_calc_found_rows * from ${ARTICLE_TABLE_NAME} where FIND_IN_SET(${categoryId}, categories) group by createTime desc limit ${limit} offset ${offset};SELECT FOUND_ROWS() as total;`);
+		return query(`select sql_calc_found_rows * from ${ARTICLE_TABLE_NAME} where FIND_IN_SET(${categoryId}, categories) order by createTime desc limit ${limit} offset ${offset};SELECT FOUND_ROWS() as total;`);
 	},
 	async getArticlePageListByTagId(params) {
 		const {tagId, limit, offset} = params;
-		return query(`select sql_calc_found_rows * from ${ARTICLE_TABLE_NAME} where FIND_IN_SET(${tagId}, tagIds) group by createTime desc limit ${limit} offset ${offset};SELECT FOUND_ROWS() as total;`);
+		return query(`select sql_calc_found_rows * from ${ARTICLE_TABLE_NAME} where FIND_IN_SET(${tagId}, tagIds) order by createTime desc limit ${limit} offset ${offset};SELECT FOUND_ROWS() as total;`);
 	},
 	async createArticle(data) {
 		let sqlStatement = `insert into ${ARTICLE_TABLE_NAME} (title, categories, tagIds, content, userId, updateTime, createTime) values (?, ?, ?, ?, ?, ?, ?)`;
@@ -47,7 +47,7 @@ const article = {
 		return query(sqlStatement, [id, data.userId, data.content, currentDate])
 	},
 	async getArticleCommentAllList() {
-		return query(`select * from ${ARTICLE_COMMENT_TABLE_NAME} group by createTime desc`)
+		return query(`select * from ${ARTICLE_COMMENT_TABLE_NAME} order by createTime desc`)
 	},
 	async getArticleCommentList(id) {
 		return query(`select a.*, b.username userName, b.profilePicture userProfilePicture from ${ARTICLE_COMMENT_TABLE_NAME} a left join ${USER_TABLE_NAME} b ON a.userId = b.id where a.articleId=${id}`)
@@ -64,7 +64,7 @@ const article = {
 		return query(`delete from ${ARTICLE_REPLY_TABLE_NAME} where id = ${replyId} ${isReply ? `or (replyWay=20 and replyId=${replyId})` : ''}`)
 	},
 	async getArticleCommentAllReplyList() {
-		return query(`select * from ${ARTICLE_REPLY_TABLE_NAME} group by createTime desc`)
+		return query(`select * from ${ARTICLE_REPLY_TABLE_NAME} order by createTime desc`)
 	},
 	async getArticleCommentReplyListByCommentId(commentId) {
 		return query(`select reply.*, user.username userName, user.profilePicture userProfilePicture, toUser.username toUserName from ${ARTICLE_REPLY_TABLE_NAME} reply left join ${USER_TABLE_NAME} user ON reply.userId = user.id left join ${USER_TABLE_NAME} toUser on reply.toUserId = toUser.id where reply.commentId=${commentId}`)
