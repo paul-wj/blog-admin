662ebf7cc9f91222c4836466d32b6354f9c1ece3
paul wang <wangj@abc-group.cn>
2019-10-10 17:59:36 +0800

    配置文件修改

--- config/index.js
+++ config/index.js
@@ -4,15 +4,15 @@ const config = {
 	port: 9000,
 	database: {
 		DATABASE: 'own_blog',
-		USERNAME: 'root',
-		PASSWORD: 'www5576081',
+		USERNAME: 'paul_wang',
+		PASSWORD: 'a2UPSr1AG*kZ3kdf',
 		PORT: '3306',
 		HOST: '118.24.181.75'
 	},
 	redis: {
 		host: '118.24.181.75',       //安装好的redis服务器地址
 		port: '6379',       //端口
-		password: 'WjRedisPASS123'
+		// password: 'WjRedisPASS123'
 	},
 	databaseNameList: {
 		ARTICLE_TABLE_NAME: 'article_info',
