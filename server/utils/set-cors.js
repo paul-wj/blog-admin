58756fdfa4c56fdf8b7a8fe79ed0465d65090268
paul wang <wangj@abc-group.cn>
2019-10-16 17:04:57 +0800

    cors根据全局环境获取对应白名单

--- server/utils/set-cors.js
+++ server/utils/set-cors.js
@@ -1,15 +1,18 @@
+const config = require('../../config/index');
 const cors = require('koa2-cors');
-const whiteList = ['http://localhost:3000', 'http://188.188.188.233:8081', 'http://118.24.181.75:3000', 'http://www.wangjie818.wang:3000'];
+const {IS_LOCAL} = config;
+const localWhiteList = ['http://localhost:3000', 'http://188.188.188.233:3000',  'http://188.188.188.233:8081'];
+const whiteList = [ 'http://118.24.181.75:3000', 'http://www.wangjie818.wang:3000'];
 const crosOptions = {
 	origin: ctx => {
-		if (ctx.request.header.origin !== ctx.origin && whiteList.includes(ctx.request.header.origin)) {
+		if (ctx.request.header.origin !== ctx.origin && (IS_LOCAL ? localWhiteList: whiteList).includes(ctx.request.header.origin)) {
 			return ctx.request.header.origin
 		}
 	},    //允许访问的来源地址
 	maxAge: 60*60,                                                                      //设置预检请求有效期（单位: s）
 	credentials: false,                                                               //是否允许传输cookie（如果为true需要保证origin为*）
 	allowMethods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'], //设置请求支持的类型
-	allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-requested-with'],                   //请求服务器支持的所有头信息字段
+	allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-requested-with', 'cip'],                   //请求服务器支持的所有头信息字段
 };
 module.exports = cors(crosOptions);
 
