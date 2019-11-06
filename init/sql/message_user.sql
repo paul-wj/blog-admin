0981511bf4145458116c9ecff7af68e7383da066
paul wang <wangj@abc-group.cn>
2019-11-06 14:36:47 +0800

    消息通知所有sql表添加comment

--- init/sql/message_user.sql
+++ init/sql/message_user.sql
@@ -1,8 +1,8 @@
 CREATE TABLE  IF NOT EXISTS `message_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) DEFAULT NULL,
-  `messageId` int(11) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL COMMENT '用户id',
+  `messageId` int(11) DEFAULT NULL COMMENT '信息id',
   `status` int(11) NULL DEFAULT 10 COMMENT '（10：已阅读）',
-  `createDate` varchar(50) DEFAULT NULL,
+  `createDate` varchar(50) DEFAULT NULL COMMENT '阅读日期',
    PRIMARY KEY (`id`)
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8;
