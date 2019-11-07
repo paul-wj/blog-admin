ea7257044a689b556a68600b607f5ddf36705eb7
paul wang <wangj@abc-group.cn>
2019-11-07 14:14:12 +0800

    log表字段属性更改

--- init/sql/log.sql
+++ init/sql/log.sql
@@ -5,9 +5,9 @@ CREATE TABLE  IF NOT EXISTS `log_info` (
   `ip` varchar(50) DEFAULT NULL,
   `host` varchar(50) DEFAULT NULL,
   `origin` varchar(50) DEFAULT NULL,
-  `userAgent` varchar(50) DEFAULT NULL,
+  `userAgent` longtext DEFAULT NULL,
   `status` varchar(20) DEFAULT NULL,
-  `url` varchar(50) DEFAULT NULL,
+  `url` longtext DEFAULT NULL,
   `method` varChar(20)  DEFAULT NULL,
   `createTime` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
