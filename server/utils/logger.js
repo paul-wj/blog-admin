832cf6442c34f23b994db5ec1fbda31ac8c3b384
paul wang <wangj@abc-group.cn>
2019-11-07 14:39:53 +0800

    日志添加操作时间显示

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,6 +1,7 @@
 const koaLogger = require('koa-logger');
 const {getTokenResult} = require('./check-token');
 const {createLogger} = require('../sql/logger');
+const {formatDate} = require('./index');
 
 module.exports = async function logger(ctx, next) {
 	const userInfo = await getTokenResult(ctx.header.authorization);
@@ -19,7 +20,7 @@ module.exports = async function logger(ctx, next) {
 		status: null
 	};
 	return koaLogger((str, args) => {
-		console.log(str);
+		console.log(str, `time:${formatDate(new Date())}`, `ip:${cip}`);
 		let status = args[3];
 		if (status) {
 			loggerData.status = status;
