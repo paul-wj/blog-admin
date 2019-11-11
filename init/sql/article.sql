c536084bcbeaf6155688a82e327556c90fad2724
paul wang <wangj@abc-group.cn>
2019-11-11 15:31:55 +0800

    添加阅读次数字段

--- init/sql/article.sql
+++ init/sql/article.sql
@@ -1,11 +1,12 @@
 CREATE TABLE  IF NOT EXISTS `article_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) DEFAULT NULL,
-  `categories` varchar(255) DEFAULT NULL,
-  `tagIds` varchar(255) DEFAULT NULL,
-  `userId` int(11) DEFAULT 1,
-  `createTime` varchar(50) DEFAULT NULL,
-  `updateTime` varchar(50) DEFAULT NULL,
-  `content` longtext DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL COMMENT '文章标题',
+  `categories` varchar(255) DEFAULT NULL COMMENT '文章目录',
+  `tagIds` varchar(255) DEFAULT NULL COMMENT '文章标签',
+  `userId` int(11) DEFAULT 1 COMMENT '创建人id',
+  `viewCount` int(11) DEFAULT 0 COMMENT '文章阅读数',
+  `createTime` varchar(50) DEFAULT NULL COMMENT '创建时间',
+  `updateTime` varchar(50) DEFAULT NULL COMMENT '更新时间',
+  `content` longtext DEFAULT NULL COMMENT '文章内容',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
